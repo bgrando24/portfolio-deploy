07f3f189caa175dfd871354347265d8cbac19ca2
Brandon <bgrando24@gmail.com>
2022-07-22 09:17:05 +1000

    Beginning of refactor

--- src/about.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import NavBar from './NavBar';
-
-ReactDOM.render(
-    <NavBar />,
-    document.getElementById('about-root')
-);
